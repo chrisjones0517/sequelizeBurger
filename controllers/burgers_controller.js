47422f7501c157d44a04ab5998812a124f467491
Chris Jones <chrisjones0517@gmail.com>
2018-05-07 21:48:43 -0500

    test

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -29,6 +29,7 @@ router.post('/', (req, res) => {
         .catch(err => {
             if (err) {
                 errors.push(err.errors[0].message);
+                res.redirect('/');
             }
         })
         .then(() => {
